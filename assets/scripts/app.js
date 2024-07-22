e3b9ae48f00659f07f3fa27d713b4d794d181840
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-07-23 00:27:16 +0200

    Adding Heal Player Functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,25 +1,15 @@
 const ATTACK_VALUE=10;
 const MONSTER_ATTACK_VALUE=14;
 const STRONG_ATTACK_VALUE=17;
+const HEAL_VALUE=20;
 let currentMonsterHealth=100;
 let currentPlayerHealth=100;
 let chosenMaxLife=100;
 adjustHealthBars(chosenMaxLife);
-
-function attackMonster(mode){
-    let maxDamage;
-    if(mode==='ATTACK'){
-        maxDamage=ATTACK_VALUE;
-    }
-    else if(mode==='STRONG_ATTACK'){
-        maxDamage=STRONG_ATTACK_VALUE;
-    }
-    const damage=dealMonsterDamage(maxDamage);
-    currentMonsterHealth-=damage;
+function endRound(){
     const playerDamage=dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth=currentPlayerHealth-playerDamage;
-
-    if (currentMonsterHealth<=0 && currentPlayerHealth>0){
+    if(currentMonsterHealth<=0 && currentPlayerHealth>0){
         alert('You won!');
     }
     else if(currentPlayerHealth<=0 && currentMonsterHealth>0){
@@ -30,11 +20,38 @@ function attackMonster(mode){
     }
 
 }
+function attackMonster(mode){
+    let maxDamage;
+    if(mode==='ATTACK'){
+        maxDamage=ATTACK_VALUE;
+    }
+    else if(mode==='STRONG_ATTACK'){
+        maxDamage=STRONG_ATTACK_VALUE;
+    }
+    const damage=dealMonsterDamage(maxDamage);
+    currentMonsterHealth-=damage;
+    endRound();
+
+}
 function attackHandler(){
     attackMonster('ATTACK');
 }
 function strongAttackHandler(){
     attackMonster('STRONG_ATTACK')
 }
+function healPlayerHandler(){
+    let healValue;
+    if(currentPlayerHealth>=chosenMaxLife-HEAL_VALUE) {
+        alert("You can't heal more than your max initial health.");
+        healValue = chosenMaxLife - currentPlayerHealth;
+    }
+    else {
+        healValue = HEAL_VALUE
+    }
+  increasePlayerHealth(HEAL_VALUE);
+  currentPlayerHealth+=HEAL_VALUE;
+  endRound();
+}
 attackBtn.addEventListener('click',attackHandler);
 strongAttackBtn.addEventListener('click',strongAttackHandler);
+healBtn.addEventListener('click',healPlayerHandler);
