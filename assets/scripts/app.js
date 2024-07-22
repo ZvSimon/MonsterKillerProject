83a3cbce29dcbdbca16f4757dfd40fcd4fe4ee4b
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-07-23 00:08:06 +0200

    Checking win condition

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,5 @@
 const ATTACK_VALUE=10;
-
+const MONSTER_ATTACK_VALUE=14;
 let currentMonsterHealth=100;
 let currentPlayerHealth=100;
 let chosenMaxLife=100;
@@ -7,5 +7,17 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler(){
     const damage=dealMonsterDamage(ATTACK_VALUE);
    currentMonsterHealth=currentMonsterHealth-damage;
+    const playerDamage=dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth=currentPlayerHealth-playerDamage;
+
+   if (currentMonsterHealth<=0 && currentPlayerHealth>0){
+       alert('You won!');
+   }
+   else if(currentPlayerHealth<=0 && currentMonsterHealth>0){
+       alert('You lost!');
+   }
+   else if(currentPlayerHealth<=0 && currentMonsterHealth<=0){
+       alert('You have a draw!');
+   }
 }
 attackBtn.addEventListener('click',attackHandler);
