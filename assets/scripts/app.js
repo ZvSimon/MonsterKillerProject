7fd8e5bb44125d5237467c46c2174937e148b2da
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-07-23 00:17:34 +0200

    Adding Strong Attack & refractoring

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,23 +1,40 @@
 const ATTACK_VALUE=10;
 const MONSTER_ATTACK_VALUE=14;
+const STRONG_ATTACK_VALUE=17;
 let currentMonsterHealth=100;
 let currentPlayerHealth=100;
 let chosenMaxLife=100;
 adjustHealthBars(chosenMaxLife);
-function attackHandler(){
-    const damage=dealMonsterDamage(ATTACK_VALUE);
-   currentMonsterHealth=currentMonsterHealth-damage;
+
+function attackMonster(mode){
+    let maxDamage;
+    if(mode==='ATTACK'){
+        maxDamage=ATTACK_VALUE;
+    }
+    else if(mode==='STRONG_ATTACK'){
+        maxDamage=STRONG_ATTACK_VALUE;
+    }
+    const damage=dealMonsterDamage(maxDamage);
+    currentMonsterHealth-=damage;
     const playerDamage=dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth=currentPlayerHealth-playerDamage;
 
-   if (currentMonsterHealth<=0 && currentPlayerHealth>0){
-       alert('You won!');
-   }
-   else if(currentPlayerHealth<=0 && currentMonsterHealth>0){
-       alert('You lost!');
-   }
-   else if(currentPlayerHealth<=0 && currentMonsterHealth<=0){
-       alert('You have a draw!');
-   }
+    if (currentMonsterHealth<=0 && currentPlayerHealth>0){
+        alert('You won!');
+    }
+    else if(currentPlayerHealth<=0 && currentMonsterHealth>0){
+        alert('You lost!');
+    }
+    else if(currentPlayerHealth<=0 && currentMonsterHealth<=0){
+        alert('You have a draw!');
+    }
+
+}
+function attackHandler(){
+    attackMonster('ATTACK');
+}
+function strongAttackHandler(){
+    attackMonster('STRONG_ATTACK')
 }
 attackBtn.addEventListener('click',attackHandler);
+strongAttackBtn.addEventListener('click',strongAttackHandler);
