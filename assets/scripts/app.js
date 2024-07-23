59727e7301e2d5fea3a2f7de4ea3c8db4760672e
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-07-23 11:05:42 +0200

    Adding a Conditional BattleLog

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,15 +4,57 @@ const STRONG_ATTACK_VALUE=17;
 const HEAL_VALUE=20
 const ATTACK_MODE='ATTACK';
 const STRONG_ATTACK_MODE='STRONG_ATTACK';
+const LOG_EVENT_PLAYER_ATTACK='PLAYER_ATTACK';
+const LOG_EVENT_PLAYER_STRONG_ATTACK='PLAYER_STRONG_ATTACK';
+const LOG_EVENT_MONSTER_ATTACK='MONSTER_ATTACK';
+const LOG_EVENT_PLAYER_HEAL='PLAYER_HEAL';
+const LOG_EVENT_GAME_OVER='GAME_OVER';
+
 const enteredValue = prompt('Enter the maximum life for you and the monster','100');
 let chosenMaxLife=parseInt(enteredValue);
+
 if (isNaN(chosenMaxLife) || chosenMaxLife<=0){
     chosenMaxLife=100;
 }
 let currentMonsterHealth=100;
 let currentPlayerHealth=100;
 let hasBonusLife=true;
+let battleLof=[];
 adjustHealthBars(chosenMaxLife);
+
+function writeToLog(ev,val,monsterHealth,playerHealth){
+    let logEntry={
+        event:ev,
+        value:val,
+        finalMonsterHealth:monsterHealth,
+        finalPlayerHealth:playerHealth
+    };
+    if(ev===LOG_EVENT_PLAYER_ATTACK){
+        logEntry.target='MONSTER';
+        
+    }
+    else if (ev===LOG_EVENT_PLAYER_STRONG_ATTACK){
+       logEntry.target='MONSTER';
+        
+    }
+    else if ( ev===LOG_EVENT_MONSTER_ATTACK){
+        logEntry.target='PLAYER';
+        
+    }else if (ev===LOG_EVENT_PLAYER_HEAL){
+       logEntry.target='PLAYER';
+        
+
+    }else if (ev===LOG_EVENT_GAME_OVER){
+        logEntry={
+            event:ev,
+            value:val,
+            finalMonsterHealth:monsterHealth,
+            finalPlayerHealth:playerHealth
+        };
+        
+    }
+    battleLof.push(logEntry);
+}
 function reset(){
     currentMonsterHealth=chosenMaxLife;
     currentPlayerHealth=chosenMaxLife;
@@ -22,6 +64,7 @@ function endRound(){
     const initialPlayerHealth=currentPlayerHealth;
     const playerDamage=dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth=currentPlayerHealth-playerDamage;
+    writeToLog(LOG_EVENT_MONSTER_ATTACK,playerDamage,currentMonsterHealth,currentPlayerHealth);
     if (currentPlayerHealth<=0 && hasBonusLife ){
         hasBonusLife=false;
         removeBonusLife();
@@ -31,14 +74,15 @@ function endRound(){
     }
     if(currentMonsterHealth<=0 && currentPlayerHealth>0){
         alert('You won!');
-
+        writeToLog(LOG_EVENT_GAME_OVER,'PLAYER WON',currentMonsterHealth,currentPlayerHealth);
     }
     else if(currentPlayerHealth<=0 && currentMonsterHealth>0){
         alert('You lost!');
-
+        writeToLog(LOG_EVENT_GAME_OVER,'MONSTER WON',currentMonsterHealth,currentPlayerHealth);
     }
     else if(currentPlayerHealth<=0 && currentMonsterHealth<=0){
         alert('You have a draw!');
+        writeToLog(LOG_EVENT_GAME_OVER,'A DRAW',currentMonsterHealth,currentPlayerHealth);
     }
     if (currentMonsterHealth<=0 || currentPlayerHealth<=0) {
         reset(chosenMaxLife);
@@ -47,14 +91,18 @@ function endRound(){
 }
 function attackMonster(mode){
     let maxDamage;
+    let logEvent;
     if(mode===ATTACK_MODE){
         maxDamage=ATTACK_VALUE;
+        logEvent=LOG_EVENT_PLAYER_ATTACK;
     }
     else if(mode===STRONG_ATTACK_MODE){
         maxDamage=STRONG_ATTACK_VALUE;
+        logEvent=LOG_EVENT_PLAYER_STRONG_ATTACK;
     }
     const damage=dealMonsterDamage(maxDamage);
     currentMonsterHealth-=damage;
+    writeToLog(logEvent,damage,currentMonsterHealth,currentPlayerHealth);
     endRound();
 
 }
@@ -75,8 +123,13 @@ function healPlayerHandler(){
     }
   increasePlayerHealth(healValue);
   currentPlayerHealth+=healValue ;
+  writeToLog(LOG_EVENT_PLAYER_HEAL,healValue,currentMonsterHealth,currentPlayerHealth);
   endRound();
+}
+function printLogHandler(){
+    console.log(battleLof);
 }
 attackBtn.addEventListener('click',attackHandler);
 strongAttackBtn.addEventListener('click',strongAttackHandler);
 healBtn.addEventListener('click',healPlayerHandler);
+logBtn.addEventListener('click',printLogHandler);
